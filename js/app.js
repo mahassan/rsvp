3332a907abbe1a0f8bbad01dd2eedeacb2c572dc
Ammar Hassan <ammarhassan@mbps-mbp.home>
2018-06-09 10:11:22 +0100

    Removing  people to the list

--- js/app.js
+++ js/app.js
@@ -2,10 +2,7 @@ const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 const ul = document.getElementById('invitedList');
 
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    const text = input.value;
-    input.value = '';
+function createLI(text){
     const li = document.createElement('li');
     li.textContent = text;
     const label = document.createElement('label');
@@ -14,6 +11,17 @@ form.addEventListener('submit', (e)=>{
     checkbox.type = 'checkbox'
     label.appendChild(checkbox);
     li.appendChild(label);
+    const button = document.createElement('button')
+    button.textContent = 'Remove'
+    li.appendChild(button);
+    return li;
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const text = input.value;
+    input.value = '';
+    const li = createLI(text);
     ul.appendChild(li);
 })
 
@@ -27,4 +35,12 @@ ul.addEventListener('change', (e) => {
     }else{
         listItem.className = ';'
     }
+
+    ul.addEventListener('click', (e) => {
+        if(e.target.tagName === 'BUTTON') {
+            const li = e.target.parentNode;
+            const ul = li.parentNode;
+            ul.removeChild(li);
+        }
+    })
 })
